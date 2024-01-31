3f59616e2eb6ea198a92bb9bfa2bcb8982312c2d
Mileymehak <102404523+Mileymehak@users.noreply.github.com>
2024-01-31 11:13:52 +0530

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -1,13 +1,13 @@
-package com.example
+package com.toon
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class TestPlugin: Plugin() {
+class ToonoPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(Toono())
+        registerMainAPI(ToonoProvider())
     }
 }
